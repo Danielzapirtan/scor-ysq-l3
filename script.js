f65fb181ad813bac23f392fee5b953d9681055c7
Danielzapirtan <137394552+Danielzapirtan@users.noreply.github.com>
2024-12-05 11:53:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,27 @@ function main() {
     [12, 13, 14],
     [15, 16, 17, 18]
   ];
-
+function getData() {
+const downloadedJson = 
+{"bakResponses":["5","5","6","1","6","3","2","4","2","2","2","4","5","5","5","6","4","3","6","5","4","6","
+3","6","5","3","4","1","5","2","1","3","5","6","3","5","5","5","2","6","4","3","4","3","6","4","4","4","5","3
+","3","4","4","4","5","1","3","1","6","3","5","4","1","4","1","4","6","4","3","3","6","1","1","2","5","2","5",
+"6","6","1","1","6","4","6","6","1","1","5","6","6","3","4","4","6","1","6","5","2","4","3","6","2","1","5","
+1","4","1","4","2","1","4","2","6","1","1","5","4","1","6","3","1","1","2","2","2","5","3","6","3","4","1","1
+","4","3","2","6","1","1","4","6","6","4","4","6","5","1","4","1","3","2","3","3","3","6","2","5","5","2","2",
+"3","3","4","1","2","6","5","6","1","4","5","5","2","5","2","5","6","4","1","3","3","5","5","3","2","2","2","
+6","3","4","3","6","6","6","1","6","3","6","3","5","1","2","1","4","3","4","3","4","1","4","5","4","4","3","3
+","2","5","2","4","5","5","6","1","1","6","1","2","2","2","2","5","6","2"],"schemaDetails":["Abandon/
+Instabilitate Alege parteneri și persoane semnificative indisponibile sau impredictibile. Alege
+parteneri care nu pot face un angajament și rămâne în relație cu aceștia. Evită orice fel de relații
+intime, din cauza fricii de abandon. bea alcool în exces atunci când este singur(ă). își
+îndepărtează partenerii și alte persoane semnificative din cauză că se agață, este posesiv sau
+controlează. Se agață și își sufocă partenerul până îl depărtează; își atacă vehement partenerul
+din cauza unor separări minore.
+","","","","","","","","","","","","","","","","",""],"domainDetails":["","","","",""]};
+const data = JSON.parse(downloadJson);
+return data;
+}
   let domainDetails;
   const dditem = JSON.parse(localStorage.getItem("dditem"));
   if (dditem && !resetAll) domainDetails = dditem;
